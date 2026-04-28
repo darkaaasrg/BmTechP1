5382b691c96537e16a1d307b5e6b879fb82d5c4e
Daryna <kisildarina09@gmail.com>
2026-04-28 15:48:40 +0300

    recent changes

---
+++ app/forms.py
@@ -0,0 +1,43 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+
+import sqlalchemy as sa
+from app import db
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat password', validators=[DataRequired(),
+                              EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = db.session.scalar(
+            sa.select(User).where(User.username == username.data)
+        )
+        if user is not None:
+            raise ValidationError('Username already in use')
+
+
+    def validate_email(self, email):
+        email = db.session.scalar(
+            sa.select(User).where(User.email == email.data)
+        )
+        if email is not None:
+            raise ValidationError('Please use a different email address')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
